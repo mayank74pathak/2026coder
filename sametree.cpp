754cc471189d2b8d2e08ecb7902420c57db090f3
mayank74pathak <56240383+mayank74pathak@users.noreply.github.com>
2026-02-14 18:47:07 +0530

    Implement isSameTree function to compare two trees

---
+++ sametree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL and q==NULL)
+        return true;
+        if((p!=NULL and q==NULL) or (p==NULL and q!=NULL))
+        return false;
+        if(p->val==q->val)
+        return isSameTree(p->left,q->left) and isSameTree(p->right,q->right);
+       return false;
+    }
+};
