7b8e5bc3d4a3081e4d17bcdbe2ee8e23c81dbbf3
mayank74pathak <56240383+mayank74pathak@users.noreply.github.com>
2026-02-21 10:34:21 +0530

    Create LCAINBST.cpp

---
+++ LCAINBST.cpp
@@ -0,0 +1,18 @@
+ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root!=NULL)
+        {
+           if(root->val>p->val and root->val>q->val)
+           {
+            root=root->left;
+           }
+           else if(root->val<p->val and root->val<q->val)
+           {
+            root=root->right;
+           }
+            else
+              {
+                return root;
+              }
+        }
+    return NULL;
+    }
