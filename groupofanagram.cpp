30d430c1aec48954740cd450a00a39b8b068b23d
Mayank Pathak <56240383+mayank74pathak@users.noreply.github.com>
2026-03-24 13:12:34 +0530

    Create groupofanagram.cpp

---
+++ groupofanagram.cpp
@@ -0,0 +1,15 @@
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        for(auto s:strs)
+        {
+            string temp=s;
+            sort(s.begin(),s.end());
+            mp[s].push_back(temp); 
+        } 
+   vector<vector<string>>ans;
+   for(auto it:mp)
+   {
+      ans.push_back(it.second);
+   }
+   return ans;
+    }
