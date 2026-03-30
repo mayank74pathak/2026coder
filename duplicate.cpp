4ab3b33f9183989acd529aafc25921cc43c76306
Mayank Pathak <56240383+mayank74pathak@users.noreply.github.com>
2026-03-30 22:50:58 +0530

    Implement function to find duplicate in vector

---
+++ duplicate.cpp
@@ -0,0 +1,17 @@
+int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int duplicate;
+        for(auto it:nums)
+        {
+            mp[it]++;
+        }
+        for(auto it:mp)
+        {
+            if(it.second>1)
+            {
+                 duplicate= it.first;
+            }
+        }
+       return duplicate;   
+    }
+};
