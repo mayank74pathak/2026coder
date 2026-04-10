23925ad496b2cb0546e140c2ff036a0db10320c2
Mayank Pathak <56240383+mayank74pathak@users.noreply.github.com>
2026-04-10 22:26:09 +0530

    Implement search in rotated sorted array

---
+++ find elementinrotatedarray.cpp
@@ -0,0 +1,34 @@
+ bool search(vector<int>& nums, int target) {
+      int l = 0, r = nums.size() - 1;
+
+    while (l <= r) {
+        int mid = (l + r) / 2;
+
+        if (nums[mid] == target)
+            return true;
+
+        // HANDLE DUPLICATES
+        else if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
+            l++;
+            r--;
+        }
+        // Left sorted
+        else if (nums[l] <= nums[mid]) {
+            if (nums[l] <= target && target < nums[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        // Right sorted
+        else {
+            if (nums[mid] < target && target <= nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+    }
+
+    return false;
+    }
