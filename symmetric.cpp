a3e6eb112e9594a337bff963096190f3a0c60dd3
Mayank Pathak <56240383+mayank74pathak@users.noreply.github.com>
2026-04-29 20:10:38 +0530

    Implement isSymmetric function for binary tree

---
+++ symmetric.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  bool check(TreeNode*p,TreeNode*q)
+{
+    if(q==NULL and p==NULL)
+    return true;
+
+    if(q== NULL or p==NULL)
+    return false;
+
+  return p->val==q->val and check(p->left,q->right) and check(p->right,q->left); 
+}
+    bool isSymmetric(TreeNode* root) {
+        bool ans ;
+        if(root==NULL)
+        return true;
+      ans=  check(root->left,root->right);
+    return ans ;
+    }
+};
