efaaab0721c0a83e95c1cbacd58d678c0e6de328
Mayank Pathak <56240383+mayank74pathak@users.noreply.github.com>
2026-05-09 14:45:04 +0530

    Create searchbst1.cpp

---
+++ searchbst1.cpp
@@ -0,0 +1,16 @@
+ TreeNode* searchBST(TreeNode* root, int val) {
+       while(root!=NULL)
+       {
+        if(root->val==val)
+        return root;
+        else if(root->val<val)
+        {
+            root=root->right;
+        }
+        else
+        {
+            root=root->left;
+        }
+       } 
+    return root;
+    }
